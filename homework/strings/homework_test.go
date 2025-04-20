82c910b2e094c9948b89a12657a508d1ba1d4261
russkih.kirill5 <Russkih.Kirill5@wb.ru>
2025-04-20 22:43:28 +0500

    hw_3

--- homework/strings/homework_test.go
+++ homework/strings/homework_test.go
@@ -2,36 +2,81 @@ package main
 
 import (
 	"reflect"
+	"sync/atomic"
 	"testing"
 	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
 
-type COWBuffer struct {
+type bufferData struct {
+	ref  int32
 	data []byte
-	refs *int
-	// need to implement
+}
+
+type COWBuffer struct {
+	d *bufferData
 }
 
 func NewCOWBuffer(data []byte) COWBuffer {
-	return COWBuffer{} // need to implement
+	return COWBuffer{&bufferData{
+		ref:  1,
+		data: data,
+	}}
 }
 
 func (b *COWBuffer) Clone() COWBuffer {
-	return COWBuffer{} // need to implement
+	if b.d != nil {
+		atomic.AddInt32(&b.d.ref, 1)
+	}
+
+	return COWBuffer{b.d}
 }
 
 func (b *COWBuffer) Close() {
-	// need to implement
+	if b.d == nil {
+		return
+	}
+
+	if atomic.AddInt32(&b.d.ref, -1) == 0 {
+		b.d.data = nil
+	}
+
+	b.d = nil
 }
 
 func (b *COWBuffer) Update(index int, value byte) bool {
-	return false // need to implement
+	if b.d == nil || index < 0 || index >= len(b.d.data) {
+		return false
+	}
+
+	if atomic.LoadInt32(&b.d.ref) > 1 {
+		newData := make([]byte, len(b.d.data))
+
+		copy(newData, b.d.data)
+
+		atomic.AddInt32(&b.d.ref, -1)
+
+		b.d = &bufferData{ref: 1, data: newData}
+	}
+
+	b.d.data[index] = value
+
+	return true
 }
 
 func (b *COWBuffer) String() string {
-	return "" // need to implement
+	if b.d == nil {
+		return ""
+	}
+
+	data := b.d.data
+	if len(data) == 0 {
+		return ""
+	}
+
+	ptr := unsafe.SliceData(data)
+	return unsafe.String(ptr, len(data))
 }
 
 func TestCOWBuffer(t *testing.T) {
@@ -42,9 +87,9 @@ func TestCOWBuffer(t *testing.T) {
 	copy1 := buffer.Clone()
 	copy2 := buffer.Clone()
 
-	assert.Equal(t, unsafe.SliceData(data), unsafe.SliceData(buffer.data))
-	assert.Equal(t, unsafe.SliceData(buffer.data), unsafe.SliceData(copy1.data))
-	assert.Equal(t, unsafe.SliceData(copy1.data), unsafe.SliceData(copy2.data))
+	assert.Equal(t, unsafe.SliceData(data), unsafe.SliceData(buffer.d.data))
+	assert.Equal(t, unsafe.SliceData(buffer.d.data), unsafe.SliceData(copy1.d.data))
+	assert.Equal(t, unsafe.SliceData(copy1.d.data), unsafe.SliceData(copy2.d.data))
 
 	assert.True(t, (*byte)(unsafe.SliceData(data)) == unsafe.StringData(buffer.String()))
 	assert.True(t, (*byte)(unsafe.StringData(buffer.String())) == unsafe.StringData(copy1.String()))
@@ -54,18 +99,18 @@ func TestCOWBuffer(t *testing.T) {
 	assert.False(t, buffer.Update(-1, 'g'))
 	assert.False(t, buffer.Update(4, 'g'))
 
-	assert.True(t, reflect.DeepEqual([]byte{'g', 'b', 'c', 'd'}, buffer.data))
-	assert.True(t, reflect.DeepEqual([]byte{'a', 'b', 'c', 'd'}, copy1.data))
-	assert.True(t, reflect.DeepEqual([]byte{'a', 'b', 'c', 'd'}, copy2.data))
+	assert.True(t, reflect.DeepEqual([]byte{'g', 'b', 'c', 'd'}, buffer.d.data))
+	assert.True(t, reflect.DeepEqual([]byte{'a', 'b', 'c', 'd'}, copy1.d.data))
+	assert.True(t, reflect.DeepEqual([]byte{'a', 'b', 'c', 'd'}, copy2.d.data))
 
-	assert.NotEqual(t, unsafe.SliceData(buffer.data), unsafe.SliceData(copy1.data))
-	assert.Equal(t, unsafe.SliceData(copy1.data), unsafe.SliceData(copy2.data))
+	assert.NotEqual(t, unsafe.SliceData(buffer.d.data), unsafe.SliceData(copy1.d.data))
+	assert.Equal(t, unsafe.SliceData(copy1.d.data), unsafe.SliceData(copy2.d.data))
 
 	copy1.Close()
 
-	previous := copy2.data
+	previous := copy2.d.data
 	copy2.Update(0, 'f')
-	current := copy2.data
+	current := copy2.d.data
 
 	// 1 reference - don't need to copy buffer during update
 	assert.Equal(t, unsafe.SliceData(previous), unsafe.SliceData(current))
